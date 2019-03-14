34af05314e4449cde6781a75897a23bd1fb69ad5
Buno Gadrat <bruno.gadrat@designvegetal.com>
2019-03-14 14:47:57 -0400

    Ajout de la description de la base de données dans les références

---
+++ consignel-scp/consignel-ref.js
@@ -0,0 +1,13 @@
+/* Programme javascript pour la documentation du Consignel */
+
+/* changement de style par un clic */
+function changeClass(elem, className1,className2){ 
+elem.className = (elem.className == className1)?className2:className1; 
+}
+
+
+/* au chargement de la page */
+function debuter(){
+
+}; /* fin de debuter */
+
